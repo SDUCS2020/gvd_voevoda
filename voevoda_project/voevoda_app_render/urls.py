ffc0f8aefcc18e7c0a6793bf1c5c8d301e0ea692
SDUCS2020 <60130677+SDUCS2020@users.noreply.github.com>
2024-10-29 20:46:46 +0500

    Add files via upload

---
+++ voevoda_project/voevoda_app_render/urls.py
@@ -0,0 +1,17 @@
+from django.urls import path
+from .import views
+
+urlpatterns = [
+    path("", views.Main.as_view(), name='main'),
+    path("players/", views.Players.as_view(), name='players'),
+    path("player/", views.Player.as_view(), name='player'),
+    path("clan/", views.Clan.as_view(), name='clan'),
+    path("event/", views.Event.as_view(), name='event'),
+    path("fight/", views.Fight.as_view(), name='fight'),
+    path("person/", views.Person.as_view(), name='person'),
+    path("presets/", views.Presets.as_view(), name='presents'),
+    path("preset/", views.Preset.as_view(), name='present'),
+    path("fights/", views.Fights.as_view(), name='fights'),
+    path("fight/", views.Fight.as_view(), name='fight'),
+    path("events/", views.Events.as_view(), name="events")
+]
